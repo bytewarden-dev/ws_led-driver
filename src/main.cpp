ea079c687712eadb3ca4af18c00ec0e2c22d9a1d
byteWarden <wardenbyte@gmail.com>
2026-01-03 22:44:32 +0100

    Init project files

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <TimerOne.h>
+
+// v3.0.2991
+
+
+void setup()
+{
+	Wire.begin();  // Start I2C as master
+	// Initialize serial communication at 9600 bits per second
+	Serial.begin(9600);
+}	
+
+
+void loop() 
+{
+
+}	
